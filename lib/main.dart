c14939439f2b54a32b925b404bc15537a3a58840
NoHive <zar.harustat@googlemail.com>
2021-05-13 15:06:16 +0200

    Cloud FireStore and DI

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:listopolis/configure_injection.dart';
 import 'package:listopolis/core/local_storage/shared_preferences_keys.dart';
 import 'package:listopolis/features/listopolis/application/active_lists/activelist_bloc.dart';
 import 'package:listopolis/features/listopolis/application/list_creation/createlist_bloc.dart';
+import 'package:listopolis/features/listopolis/application/online_lists/onlinelists_bloc.dart';
 import 'package:listopolis/features/listopolis/application/templates/template_bloc.dart';
 import 'package:listopolis/features/listopolis/data/datasources/data_source.dart';
 import 'package:listopolis/features/listopolis/data/datasources/local_storage.dart';
@@ -12,73 +13,46 @@ import 'package:listopolis/features/listopolis/presentation/active_lists/active_
 import 'package:listopolis/features/listopolis/presentation/color_constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 // Future<SharedPreferences> init() async {
-  
+
 //   return Future.value(await SharedPreferences.getInstance());
 // }
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- // SharedPreferences prefs = await init();
+  // SharedPreferences prefs = await init();
   //runApp(ListopolisRoot(prefs));
   await configureInjection("std");
   runApp(ListopolisRoot());
 }
 
 class ListopolisRoot extends StatelessWidget {
- // final SharedPreferences preferences;
-  const ListopolisRoot( {Key? key}) : super(key: key);
-  
+  // final SharedPreferences preferences;
+  const ListopolisRoot({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     
-    //  final RepositoryImpl userDateRepo= RepositoryImpl(
-    //                                       dataSource: LocalDataSourceImpl(
-    //                                                     preferenceKey: SharedPreferencesKey.USER_DATA_KEY,
-    //                                                     sharedPreferences: preferences
-    //                                                   ),
-    //                                       backupDataSource: BackupDataSource(),
-    //                                     );
-
-    return MaterialApp(
-      title: "Listopolis",
-      color: ListColors.APP_BAR_COLOR,
-      home: MultiBlocProvider(
-            providers: [ 
-              BlocProvider<ActivelistBloc> (
+    return MultiBlocProvider(
+      providers: [
+              BlocProvider<ActivelistBloc>(
                 create: (context) => getIt<ActivelistBloc>(),
               ),
-              BlocProvider<TemplateBloc> (
+              BlocProvider<TemplateBloc>(
                 create: (context) => getIt<TemplateBloc>(),
               ),
-              BlocProvider<CreatelistBloc> (
+              BlocProvider<CreatelistBloc>(
                 create: (context) => getIt<CreatelistBloc>(),
-                                        
-                                      
               ),
-              
+              BlocProvider<OnlinelistsBloc>(
+                create: (context) => getIt<OnlinelistsBloc>(),
+              ),
             ],
-            child: ActiveListMainPage(),
-          )
+      child: MaterialApp(
+          title: "Listopolis",
+          color: ListColors.APP_BAR_COLOR,
+          home: ActiveListMainPage(),
+          ),
     );
   }
-  //  @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     title: "Listopolis",
-  //     home: MultiBlocProvider(
-  //           providers: [ 
-  //             BlocProvider<ActivelistBloc> (
-  //               create: (context) => ActivelistBloc(repository: DemoRepositoryImpl()),
-  //             ),
-  //             BlocProvider<CreatelistBloc> (
-  //               create: (context) => CreatelistBloc(repository: DemoRepositoryImpl()),
-  //             ),
-  //           ],
-  //           child: ActiveListMainPage(),
-  //         )
-  //   );
-  // }
-
 }
